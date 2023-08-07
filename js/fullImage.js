5527601302e15e21a9a408e8d84490e139487e80
Илья Панов <panoviliy@gmail.com>
2023-08-07 15:28:40 +0500

    loaded image

--- js/fullImage.js
+++ js/fullImage.js
@@ -1,8 +1,50 @@
 import { getRandomInt, isEscEvent } from './utils.js';
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 let fullImage = document.querySelector('.big-picture');
 let commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 let commentList = document.querySelector('.social__comments');
 let fullImageClose = document.querySelector('.big-picture__cancel');
+const commentsCount = document.querySelector('.social__comment-count');
+const commentsLoader = document.querySelector('.comments-loader');
+const previewImage = document.querySelector('.img-upload__preview > img');
+const uploadedImage = document.querySelector('.img-upload__input');
+const COMMENTS_LOAD_STEP = 5;
+
+uploadedImage.addEventListener('change', () => {
+  const file = uploadedImage.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it)=>{
+    return fileName.endsWith(it);
+  });
+  if (matches) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      previewImage.src = reader.result;
+    });
+
+    reader.readAsDataURL(file);
+  }
+});
+
+let commentsCounter = COMMENTS_LOAD_STEP;
+
+let commentsLoaded = [];
+const addComments = () => {
+  let commentListFragment = document.createDocumentFragment();
+  let newComments = commentsLoaded.slice(commentsCounter, commentsCounter + COMMENTS_LOAD_STEP);
+  if (newComments) {
+    newComments.forEach(comment => {
+      commentListFragment.appendChild(renderComment(comment));
+    });
+  }
+  commentsCounter += newComments.length;
+  commentsCount.textContent = `${commentsCounter} из ${commentsLoaded.length} комментариев`;
+  commentList.appendChild(commentListFragment);
+};
+
+commentsLoader.addEventListener('click', addComments);
 
 const onPopupEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
@@ -17,9 +59,11 @@ let closePicture = function() {
   fullImageClose.removeEventListener('click', closePicture);
   document.removeEventListener('keydown', onPopupEscKeydown);
   commentList.innerHTML = '';
+  commentsCounter = COMMENTS_LOAD_STEP;
+  commentsLoaded = []
 }
 
-let showPicture = function({id,url,description,likes,comments}) {
+let showPicture = function({url,description,likes,comments}) {
   let picture = fullImage.querySelector('.big-picture__img > img');
   picture.src = url;
   picture.alt = 'simple text';
@@ -28,7 +72,8 @@ let showPicture = function({id,url,description,likes,comments}) {
   social.querySelector('.social__caption').textContent = description;
   social.querySelector('.likes-count').textContent = likes;
   social.querySelector('social__comments');
-  renderComments(comments);
+  commentsLoaded = comments;
+  renderComments(comments.slice(0, commentsCounter));
   fullImage.classList.remove('hidden');
   document.body.classList.add('modal-open');
   fullImageClose.addEventListener('click', closePicture);
@@ -38,12 +83,12 @@ let showPicture = function({id,url,description,likes,comments}) {
 let renderComments = function(comments) {
   let commentListFragment = document.createDocumentFragment();
   if (comments) {
-
-
     comments.forEach(comment => {
       commentListFragment.appendChild(renderComment(comment));
     });
   }
+
+  commentsCount.textContent = `${commentsCounter} из ${commentsLoaded.length} комментариев`;
   commentList.appendChild(commentListFragment);
 }
 let renderComment = function ({avatar, name, message}) {
